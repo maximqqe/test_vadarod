67071a2e1880f84d90b1956d00e753c283347251
Romanovich Maxim <maximqq123@gmail.com>
2024-06-11 17:46:35 +0300

    Create Rate model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    Curr_ID = models.IntegerField()
+    Date = models.DateTimeField()
+    Cur_Abbreviation = models.CharField(max_length=3)
+    Cur_Scale = models.SmallIntegerField()
+    Cur_name = models.CharField(max_length=50)
+    Cur_OfficialRate = models.FloatField()
+
+    class Meta:
+        db_table = 'rate'
+        verbose_name = 'rate'
+        verbose_name_plural = 'rates'
