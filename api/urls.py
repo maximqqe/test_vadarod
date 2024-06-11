6d64399ef823cbec46dffa4ab7e98188de3ede3e
Romanovich Maxim <maximqq123@gmail.com>
2024-06-11 19:06:56 +0300

    Create endpoint to get rate

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import FetchRates
+from api.views import FetchRates, GetRate
 
 urlpatterns = [
-    path('fetch-rates/', FetchRates.as_view())
+    path('fetch-rates/', FetchRates.as_view()),
+    path('get-rate/', GetRate.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
+import datetime
 import pdb
 
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RateSerializer
+from .models import Rate
 
 
 class FetchRates(APIView):
@@ -24,3 +26,24 @@ class FetchRates(APIView):
         else:
             return Response(status=response.status_code)
 
+
+class GetRate(APIView):
+    def get(self, request):
+        date = request.GET.get('date')
+        curr_code = request.GET.get('curr_code')
+
+        if not date:
+            return Response({"error": "date required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not curr_code:
+            return Response({"error": "curr_code required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            date_obj = datetime.datetime.fromisoformat(date)
+        except Exception:
+            return Response({"error": "invalid date format"}, status=status.HTTP_400_BAD_REQUEST)
+
+        rate = get_object_or_404(Rate, Date=date_obj, Cur_Abbreviation=curr_code)
+        serializer = RateSerializer(rate)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
